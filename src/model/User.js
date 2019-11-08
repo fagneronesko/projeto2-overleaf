19a84f9a44efed72c9df5535e2478ae2d89d6a34
FagnerOnesko <fagneronesko@alunos.utfpr.edu.br>
2019-11-08 16:03:34 -0300

    Add novo BD

--- src/model/User.js
+++ src/model/User.js
@@ -14,14 +14,14 @@ class User{
     }
 
     static async find (email) {
-        let conn = await client.connect('mongodb+srv://admin:1234@cluster0-tql9j.mongodb.net/projeto2-overleaf?retryWrites=true&w=majority',
+        let conn = await client.connect('mongodb+srv://admin:1234@cluster0-f89hc.mongodb.net/test?retryWrites=true&w=majority',
             {useNewUrlParser: true, useUnifiedTopology: true});
         let db = conn.db();
         return await db.collection('users').find({email : email}).toArray();
     }
     
     static async save (user) {
-        let conn = await client.connect('mongodb+srv://admin:1234@cluster0-tql9j.mongodb.net/projeto2-overleaf?retryWrites=true&w=majority',
+        let conn = await client.connect('mongodb+srv://admin:1234@cluster0-f89hc.mongodb.net/test?retryWrites=true&w=majority',
             {useNewUrlParser: true, useUnifiedTopology: true});
         let db = conn.db();
         return await db.collection('users').insertOne(user);
